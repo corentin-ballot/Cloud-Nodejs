12dead9d0d3cb028611880a6edd9baaa34f3a6f9
Corentin Ballot <corentin.ballot-ext@socgen.com>
2019-01-23 12:16:50 +0100

    feat : add user data update

--- controllers/security-admin-controller.js
+++ controllers/security-admin-controller.js
@@ -16,9 +16,50 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/users', function (req, res) {
+    db.all('SELECT * FROM users', function (err, rows) {
+        res.render('security/admin/users.html', { users: rows });
+    });
+});
+
 app.get('/user/:id', function (req, res) {
     db.get('SELECT * FROM users WHERE id = ?', req.params.id, function (err, row) {
         res.render('security/admin/user.html', { userinfo: row });
+    });
+});
+
+app.post('/user/:id/update-username', function (req, res) {
+    db.get('UPDATE users SET username = ? WHERE id = ?', req.body.username, req.params.id, function (err, row) {
+        // redirect user
+        res.redirect('.');
+    });
+});
+
+app.post('/user/:id/update-email', function (req, res) {
+    db.get('UPDATE users SET email = ? WHERE id = ?', req.body.email, req.params.id, function (err, row) {
+        // redirect user
+        res.redirect('.');
+    });
+});
+
+app.post('/user/:id/update-roles', function (req, res) {
+    db.get('UPDATE users SET roles = ? WHERE id = ?', req.body.roles, req.params.id, function (err, row) {
+        // redirect user
+        res.redirect('.');
+    });
+});
+
+app.post('/user/:id/update-enable', function (req, res) {
+    db.get('UPDATE users SET enable = ? WHERE id = ?', req.body.enable ? 1 : 0, req.params.id, function (err, row) {
+        // redirect user
+        res.redirect('.');
+    });
+});
+
+app.post('/user/:id/delete-user', function (req, res) {
+    db.run('DELETE FROM users WHERE id = ?', req.params.id, function (err, row) {
+        // redirect users
+        res.redirect('../../users');
     });
 });
 
